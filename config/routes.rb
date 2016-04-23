233fbce359e3fb023fce1a93a60f3716546ebd29
spyrosavl <spyrosavl@gmail.com>
2016-04-23 21:41:03 +0300

    gfdgd

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   get 'auth', to: 'users#authenticate' 
   get 'select_pin', to: 'users#select_pin' 
+  post 'select_pin', to: 'users#select_pin' 
+  post 'set_pin', to: 'users#set_pin' 
 end
