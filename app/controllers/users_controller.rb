aa298590424d481278f31eac07d60791697e74b5
spyrosavl <spyrosavl@gmail.com>
2016-04-23 22:49:37 +0300

    gfdgd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     u = User.find(params[:uid])
     u.update(pin: params[:pin])
     u.save
+    render 'success_auth'
   end
   # GET /users
   # GET /users.json
