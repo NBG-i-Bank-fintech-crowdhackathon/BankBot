9cebc79c561d1810ca36a761b3f15263ef89f636
Ioannis Chatziagapis <tsepoylas@gmail.com>
2016-04-23 18:42:07 +0300

    FD

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+
 class Message < ActiveRecord::Base
 	belongs_to :user
 	@@fb_token = 'CAAW46Pf7Xo4BANlo4Unadya2BeLtUt3CO5hohlqPbn1ZCrLwbGwKQCdBQrjNFaWp0ilYlt1A4hBKebRuZA0Rai4R1wIfAsMdn3DG0jGoea2iU2frQbCcO25LQ9VEqtqMvdT07G8BbxEAXfBiOqy3NfP12t7rWp0gU7h6hRT9mPSqNfehST1fARJf2oXpBewLwMmuZBKdAZDZD'
@@ -35,9 +37,13 @@ def handle_sound()
 		:client_access_token => @@ai_token,
 		:subscription_key => 'YOUR_SUBSCRIPTION_KEY'
 		)
-	file = File.new text
+	filename = self.user.fb_id+Time.now.getutc.to_s
+	open(filename, 'wb') do |file|
+	  file << open(text).read
+	end
 	apiresponce = client.voice_request file , :contexts => [self.user.state], :sessionId => self.user.fb_id, :resetContexts => self.user.clear_state
 	puts apiresponce
+	File.delete(filename) if File.exist?(filename)
 end
 
 def answer_new(text)
